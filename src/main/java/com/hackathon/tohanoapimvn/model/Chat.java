d59e512c0ee8d6fea7b5150a47be84c01707ae14
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2024-02-10 00:36:48 +0300

    chore: initiate notification

--- src/main/java/com/hackathon/tohanoapimvn/model/Chat.java
+++ src/main/java/com/hackathon/tohanoapimvn/model/Chat.java
@@ -1,4 +1,28 @@
 package com.hackathon.tohanoapimvn.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.ListResourceBundle;
+@Data
+@NoArgsConstructor
+@Entity
 public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<User> participants;
+
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Message> message;
 }
--- src/main/java/com/hackathon/tohanoapimvn/model/Message.java
+++ src/main/java/com/hackathon/tohanoapimvn/model/Message.java
@@ -1,4 +1,29 @@
 package com.hackathon.tohanoapimvn.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private User sender;
+
+    @OneToOne
+    private User receiver;
+    private String content;
+    private LocalDate timestamp;
 }
